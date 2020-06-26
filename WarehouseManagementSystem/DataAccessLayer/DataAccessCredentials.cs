cf51905a5fb454cb0db1444f59c0d39d97486c2c
Srinivas <67001639+reddy169@users.noreply.github.com>
2020-06-26 16:40:44 +0530

    Added Model Class Admin Added Added LoginCheck method in DataAccessCredentials

---
+++ WarehouseManagementSystem/DataAccessLayer/DataAccessCredentials.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WarehouseManagementSystem.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.Common;
+
+namespace WarehouseManagementSystem.DataAccessLayer
+{
+    public class DataAccessCredentials
+    {
+        public List<Admin> LoginCheck(string uid, string email, string password, string logintype)
+        {
+            DataSet ds;
+            SqlDataAdapter da;
+            List<Admin> objAdmin = null;
+            List<Supplier> objSupp = null;
+            List<SubIncharge> objSubIn = null;
+            SqlCommand cmd;
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Warehouse"].ConnectionString))
+            {
+                
+
+                    if (logintype == "Admin")
+                    {
+                    try
+                    {
+                        cmd = new SqlCommand("CheckLogin", conn);
+                        {
+                            cmd.CommandType = CommandType.StoredProcedure;
+                        }
+
+                        cmd.Parameters.AddWithValue("@UserId", uid);
+                        cmd.Parameters.AddWithValue("@Email", email);
+                        cmd.Parameters.AddWithValue("@Password", password);
+                        cmd.Parameters.AddWithValue("@UserType", logintype);
+                        da = new SqlDataAdapter(cmd);
+                        ds = new DataSet();
+                        da.Fill(ds);
+                        objAdmin = new List<Admin>();
+                        for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                        {
+                            Admin admin = new Admin
+                            {
+                                UserId = Convert.ToInt32(ds.Tables[0].Rows[i]["UserId"].ToString()),
+                                UserName = ds.Tables[0].Rows[i]["UserName"].ToString(),
+                                FullName = ds.Tables[0].Rows[i]["FullName"].ToString(),
+                                Password = ds.Tables[0].Rows[i]["LastName"].ToString(),
+                                Email = ds.Tables[0].Rows[i]["EmailId"].ToString(),
+                                ProfileImage = ds.Tables[0].Rows[i]["Address"].ToString(),
+                                Gender = ds.Tables[0].Rows[i]["Gender"].ToString(),
+                                PhnoNo = Convert.ToInt64(ds.Tables[0].Rows[i]["PhnoNo"].ToString()),
+                                PanCardNo = ds.Tables[0].Rows[i]["PanCardNo"].ToString(),
+                                DOB = Convert.ToDateTime(ds.Tables[0].Rows[i]["DOB"].ToString()),
+                                AddressLine1 = ds.Tables[0].Rows[i]["AddressLine1"].ToString(),
+                                AddressLine2 = ds.Tables[0].Rows[i]["AddressLine2"].ToString(),
+                                Country = ds.Tables[0].Rows[i]["Country"].ToString(),
+                                State = ds.Tables[0].Rows[i]["State"].ToString(),
+                                District = ds.Tables[0].Rows[0]["District"].ToString(),
+                                ZipCode = Convert.ToInt32(ds.Tables[0].Rows[0]["ZipCode"].ToString()),
+                                CreatedBy = ds.Tables[0].Rows[0]["CreatedBy"].ToString(),
+                                CreatedOn = Convert.ToDateTime(ds.Tables[0].Rows[0]["CreatedOn"].ToString()),
+                                DeletedOn = Convert.ToDateTime(ds.Tables[0].Rows[0]["DeletedOn"].ToString()),
+                                UpdatedOn = Convert.ToDateTime(ds.Tables[0].Rows[0]["UpdatedOn"].ToString()),
+                                AccountStatus = Convert.ToBoolean(ds.Tables[0].Rows[0]["AccountStatus"].ToString()),
+                                LoginStatus = Convert.ToBoolean(ds.Tables[0].Rows[0]["LoginStatus"].ToString()),
+                                LoginTime = Convert.ToDateTime(ds.Tables[0].Rows[0]["LoginTime"].ToString()),
+                                LogOutTime = Convert.ToDateTime(ds.Tables[0].Rows[0]["LogOutTime"].ToString())
+                            };
+                            objAdmin.Add(admin);
+                        }
+                        return objAdmin;
+                    }
+                   
+                    catch
+                    {
+                        return objAdmin;
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+
+                    }
+              
+            }
+
+        }
+    }
+}
---
+++ WarehouseManagementSystem/Models/Admin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WarehouseManagementSystem.Models
+{
+    public class Admin
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string ProfileImage { get; set; }
+        public string Gender { get; set; }
+        public long PhnoNo { get; set; }
+        public DateTime DOB { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string District { get; set; }
+        public string PanCardNo { get; set; }
+        public int ZipCode { get; set; }
+        public DateTime LoginTime{get;set;}
+        public DateTime LogOutTime { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn{ get; set; }
+        public DateTime DeletedOn { get; set; }
+        public Boolean LoginStatus { get; set; }
+        public Boolean AccountStatus { get; set; }
+        public string CreatedBy { get; set; }
+        public List<Admin> AdminDetails{ get; set; }
+
+    }
+}
